35a62b0b6070ee5f9d53918849ed4235c039a483
Christian Margreitter <christian.margreitter@gmail.com>
2019-07-30 20:53:08 +0200

    made a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+       name="viennaptm",
+       version="0.0.1",
+       description="Python package to add post-translational modifications to protein structures",
+       long_description=long_description,
+       long_description_content_type="text/markdown",
+       url="http://vienna-ptm.univie.ac.at",
+       author="Christian Margreitter, Sophie Margreitter, Bojan Zagrovic",
+       author_email="christian.margreitter@gmail.com",
+       packages=setuptools.find_packages(),
+           classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
