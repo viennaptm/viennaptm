a028a8e9ec348103d005c122efca088ace87eb86
Christian Margreitter <christian.margreitter@gmail.com>
2019-09-18 21:06:57 +0200

    small fixes

--- IOclasses/io_structures.py
+++ IOclasses/io_structures.py
@@ -2,8 +2,8 @@ import os
 import shutil
 from Bio.PDB import PDBParser, PDBList
 
-class io_PDB:
-    """Class to handle PDB files"""
+class IO_structure:
+    """Class to handle structure input files"""
     def __init__(self, inp):
         if not isinstance(inp, str):
             raise AttributeError("Currently, only PDB URLs and file paths are supported.")
@@ -17,3 +17,8 @@ class io_PDB:
             path = downloader.retrieve_pdb_file(pdb_code=inp)
             _structure = parser.get_structure(id=os.path.basename(path), file=path)
             shutil.rmtree(os.path.dirname(path))
+
+        self._structure = _structure
+
+    def get_structure(self):
+        return self._structure
