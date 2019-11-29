04fdbb4355cdbd2c8a61fb6a52763ffe43ce0cfc
Christian Margreitter <christian.margreitter@gmail.com>
2019-11-29 23:35:50 +0100

    Replaced magic strings in the IO-library class with "Enum" values and refractured the code a bit.

--- IOclasses/iomodlibrary.py
+++ IOclasses/iomodlibrary.py
@@ -1,100 +1,66 @@
 import os
 from xml.dom import minidom
 
-
 from modification.modification import *
 from modification.mod_library import ModLibrary
 from utils.paths import move_directory_up
+
+from utils.enums.io_enums import IOModificationEnum
 
 
 class IOModLibrary:
     """Input / output class for modification XML libraries"""
     def __init__(self):
-        pass
+        self._IM = IOModificationEnum()
 
-    @staticmethod
-    def load_database(path=None):
+    def load_database(self, path=None):
         """Function loads an input XML file and creates and returns modification entries in a library collection."""
         # load the latest internal library, if none has been specified explicitly
         if path is None:
-            path = os.path.join(move_directory_up(__file__), "libraries")
+            path = os.path.join(move_directory_up(__file__), self._IM.INTERNAL_LIBRARY_PATH)
             files = os.listdir(path)
-            files = {x for x in files if x.endswith("_library.xml")}
+            files = {x for x in files if x.endswith(self._IM.INTERNAL_LIBRARY_SUFFIX)}
             path = os.path.join(path, sorted(files, reverse=True)[0])
 
         # load the XML and initialize the modification library
-        xml_lib = minidom.parse(path).getElementsByTagName("library")[0]
-        libObj = ModLibrary(last_update_date=xml_lib.attributes["last_update"].value,
-                            version=xml_lib.attributes["version"].value)
+        xml_lib = minidom.parse(path).getElementsByTagName(self._IM.LIBRARY)[0]
+        libObj = ModLibrary(last_update_date=xml_lib.attributes[self._IM.LIBRARY_LAST_UPDATE].value,
+                            version=xml_lib.attributes[self._IM.LIBRARY_VERSION].value)
 
         # iterate over the modifications and residues and fill the library
-        residues = xml_lib.getElementsByTagName("residue")
+        residues = xml_lib.getElementsByTagName(self._IM.RESIDUE)
         for residue in residues:
-            initial_abbreviation = residue.getAttribute("abbreviation").upper()
-            initial_name = residue.getAttribute("name").lower()
-            modifications = residue.getElementsByTagName("modification")
-            for modi in modifications:
-                modification_name = modi.getAttribute("name").upper()
-                target_abbreviation = modi.getAttribute("target_abbreviation").upper()
-                target_name = modi.getAttribute("target_name").lower()
+            initial_abbreviation = residue.getAttribute(self._IM.RESIDUE_ABBREVIATION).upper()
+            initial_name = residue.getAttribute(self._IM.RESIDUE_NAME).lower()
+            modifications = residue.getElementsByTagName(self._IM.MODIFICATION)
+            for modification in modifications:
+                modification_name = modification.getAttribute(self._IM.MODIFICATION_NAME).upper()
+                target_abbreviation = modification.getAttribute(self._IM.TARGET_ABBREVIATION).upper()
+                target_name = modification.getAttribute(self._IM.TARGET_NAME.lower())
 
                 # get the anchor atom
                 anchor = None
                 try:
-                    anchor = modi.getElementsByTagName("anchor")[0].firstChild.nodeValue.upper()
+                    anchor = modification.getElementsByTagName(self._IM.ANCHOR)[0].firstChild.nodeValue.upper()
                 except IndexError:
                     pass
 
                 # get the relative coordinate axes
                 axesDict = {}
                 try:
-                    axes = modi.getElementsByTagName("axes")[0].getElementsByTagName("axis")
+                    axes = modification.getElementsByTagName(self._IM.AXES)[0].getElementsByTagName(self._IM.AXIS)
                     for axis in axes:
-                        key = "".join(["axis", axis.getAttribute("number")])
-                        axesDict[key] = Axis(number=axis.getAttribute("number"),
-                                             p1 = axis.getAttribute("p1").upper(),
-                                             p2 = axis.getAttribute("p2").upper())
+                        key = "".join([self._IM.AXIS, axis.getAttribute(self._IM.AXIS_NUMBER)])
+                        axesDict[key] = Axis(number=axis.getAttribute(self._IM.AXIS_NUMBER),
+                                             p1=axis.getAttribute(self._IM.AXIS_POINT1).upper(),
+                                             p2=axis.getAttribute(self._IM.AXIS_POINT2).upper())
                 except IndexError:
                     pass
 
-                # get the atom additions
-                atom_additions = []
-                try:
-                    additions = modi.getElementsByTagName("additions")[0].getElementsByTagName("add")
-                    for addition in additions:
-                        atom_additions.append(AtomAddition(name=addition.getAttribute("name").upper(),
-                                                           eletype=addition.getAttribute("eletype").upper(),
-                                                           xcoorr=addition.getAttribute("xcoorr"),
-                                                           ycoorr=addition.getAttribute("ycoorr"),
-                                                           zcoorr=addition.getAttribute("zcoorr"),
-                                                           tempfactor=addition.getAttribute("tempfactor")))
-                except IndexError:
-                    pass
-
-                # get the atom deletions
-                atom_deletions = []
-                try:
-                    deletions = modi.getElementsByTagName("deletions")[0].getElementsByTagName("del")
-                    for deletion in deletions:
-                        atom_deletions.append(AtomDeletion(name=deletion.getAttribute("name").upper()))
-                except IndexError:
-                    pass
-
-                # get the atom replacements
-                atom_replacements = []
-                try:
-                    replacements = modi.getElementsByTagName("replacements")[0].getElementsByTagName("rep")
-                    for replacement in replacements:
-                        new_eletype = None
-
-                        # accessing attributes that are not present returns an empty string
-                        if replacement.getAttribute("new_eletype") != "":
-                            new_eletype = replacement.getAttribute("new_eletype").upper()
-                        atom_replacements.append(AtomReplacement(name=replacement.getAttribute("name").upper(),
-                                                                 by=replacement.getAttribute("by").upper(),
-                                                                 new_eletype=new_eletype))
-                except IndexError:
-                    pass
+                # get the three types of atom manipulations as lists
+                atom_additions = self._get_atom_additions(modification=modification)
+                atom_deletions = self._get_atom_deletions(modification=modification)
+                atom_replacements = self._get_atom_replacements(modification=modification)
 
                 new_modification = Modification(initial_abbreviation=initial_abbreviation,
                                                 initial_name=initial_name,
@@ -102,11 +68,53 @@ class IOModLibrary:
                                                 target_abbreviation=target_abbreviation,
                                                 target_name=target_name,
                                                 anchor=anchor,
-                                                axis1=axesDict["axis1"],
-                                                axis2=axesDict["axis2"],
+                                                axis1=axesDict[self._IM.AXIS1],
+                                                axis2=axesDict[self._IM.AXIS2],
                                                 atom_additions=atom_additions,
                                                 atom_deletions=atom_deletions,
                                                 atom_replacements=atom_replacements)
                 libObj.add_modification(new_modification)
 
         return libObj
+
+    def _get_atom_additions(self, modification: minidom.Element) -> list:
+        atom_additions = []
+        try:
+            additions = modification.getElementsByTagName(self._IM.ADDITIONS)[0].getElementsByTagName(self._IM.ADD)
+            for addition in additions:
+                atom_additions.append(AtomAddition(name=addition.getAttribute(self._IM.ADD_ATOM_NAME).upper(),
+                                                   eletype=addition.getAttribute(self._IM.ADD_ATOM_ELETYPE).upper(),
+                                                   xcoorr=addition.getAttribute(self._IM.ADD_ATOM_XCOORR),
+                                                   ycoorr=addition.getAttribute(self._IM.ADD_ATOM_YCOORR),
+                                                   zcoorr=addition.getAttribute(self._IM.ADD_ATOM_ZCOORR),
+                                                   tempfactor=addition.getAttribute(self._IM.ADD_ATOM_TEMPFACTOR)))
+        except IndexError:
+            pass
+        return atom_additions
+
+    def _get_atom_deletions(self, modification: minidom.Element) -> list:
+        atom_deletions = []
+        try:
+            deletions = modification.getElementsByTagName(self._IM.DELETIONS)[0].getElementsByTagName(self._IM.DEL)
+            for deletion in deletions:
+                atom_deletions.append(AtomDeletion(name=deletion.getAttribute(self._IM.DEL_ATOM_NAME).upper()))
+        except IndexError:
+            pass
+        return atom_deletions
+
+    def _get_atom_replacements(self, modification: minidom.Element) -> list:
+        atom_replacements = []
+        try:
+            replacements = modification.getElementsByTagName(self._IM.REPLACEMENTS)[0].getElementsByTagName(self._IM.REP)
+            for replacement in replacements:
+                new_eletype = None
+
+                # accessing attributes that are not present returns an empty string
+                if replacement.getAttribute(self._IM.REP_NEW_ELETYPE) != "":
+                    new_eletype = replacement.getAttribute(self._IM.REP_NEW_ELETYPE).upper()
+                atom_replacements.append(AtomReplacement(name=replacement.getAttribute(self._IM.REP_ATOM_NAME).upper(),
+                                                         by=replacement.getAttribute(self._IM.REP_BY_NAME).upper(),
+                                                         new_eletype=new_eletype))
+        except IndexError:
+            pass
+        return atom_replacements
